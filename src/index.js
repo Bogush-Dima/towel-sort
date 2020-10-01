f5ba27adbc46de809d8892cedf0871f41567a16e
Bogush-Dima <bogusd66@gmail.com>
2020-10-01 11:13:26 +0300

    feat: my decition this task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix === undefined) {
+        return []
+    } else {
+        const result = []
+    
+    for (let i = 0; i < matrix.length; i += 1) {
+        if (i % 2 !== 0) {
+            matrix[i].sort((a, b) => b - a)
+        }
+        for (let a = 0; a < matrix[i].length; a += 1) {
+            result.push(matrix[i][a])
+        }
+    }
+        return result
+    }
 }
